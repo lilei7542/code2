ba333a2d47628a1265d7db0f20b9a4a1d643aba4
lilei7542 <lilei7542@163.com>
2023-08-24 16:51:06 +0800

    all thing down ,1.0 to be test

---
+++ file/test.py
@@ -0,0 +1,43 @@
+from CaculateWithList import CaculateWithList
+a = [1,2,31,61,9]
+b = [2,31,4,5,61,71]
+
+result = CaculateWithList.list_a_and_b(a,b)
+
+result.sort()
+
+result2 = (a+b)
+print(result2)
+
+#
+# if [ -f "D:\code\code\code2\message_ass.db.03.txt" ];then
+#     echo "文件存在"
+# else
+#     echo "文件不存在"
+#
+
+
+import os
+
+if (os.path.exists('message_ass.db.03.txt')):
+    print(1)
+else:
+    print(2)
+
+
+from configparser import ConfigParser
+parser = ConfigParser()
+parser.read('config_global.ini',encoding='utf-8')
+filepath_ori = parser.get('file','filepath_ori')
+
+print(filepath_ori)
+
+
+print('/')
+
+
+s = 'one two one two one'
+
+print(s.replace(' ', '-'))
+
+print(s)
